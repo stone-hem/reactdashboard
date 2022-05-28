6ab527835f9ab8a58cc551586304039626886bcc
stone-hem <hemie@protonmail.ch>
2022-05-28 18:04:50 +0300

    table

--- src/pages/Table.js
+++ src/pages/Table.js
@@ -4,7 +4,31 @@ import styledComponents from 'styled-components'
 function Table() {
     return (
         <Section>
-            <div>Table</div>
+            <table>
+                <caption>This is my table</caption>
+                <thead>
+                    <tr>
+                        <th>header 1</th>
+                        <th>header 2</th>
+                        <th>header 3</th>
+                        <th>header 4</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>header 1</td>
+                        <td>header 2</td>
+                        <td>header 3</td>
+                        <td>header 4</td>
+                    </tr>
+                    <tr>
+                        <td>header 1</td>
+                        <td>header 2</td>
+                        <td>header 3</td>
+                        <td>header 4</td>
+                    </tr>
+                </tbody>
+            </table>
         </Section>
     )
 }
@@ -18,4 +42,12 @@ background:#1e1e1e;
 width:950px;
 height:170px;
 margin-left:62px;
+table{
+    border:3px solid yellow;
+    width:50%;
+}
+th,td{
+    padding:15px;
+    text-align:center;
+}
 `
